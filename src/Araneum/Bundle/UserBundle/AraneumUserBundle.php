f0151b9224f8cc228e5e4da66f5177e59bcfa55c
dmytrome <dmytrom@migfin.com>
2015-09-16 16:20:11 +0300

    AC-22 Подключение FOSUserBundle

--- src/Araneum/Bundle/UserBundle/AraneumUserBundle.php
+++ src/Araneum/Bundle/UserBundle/AraneumUserBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AraneumUserBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+
 }
---
+++ src/Araneum/Bundle/UserBundle/Entity/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Araneum\Bundle\UserBundle\Entity;
+
+use \FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="araneum_user")
+ */
+class User extends BaseUser
+{
+    use \Araneum\BaseBundle\EntityTrait\DateTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", name="full_name", nullable=true)
+     */
+    private $fullName;
+
+    /**
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * @param string $fullName
+     *
+     * @return User
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+}
