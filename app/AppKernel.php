874b1991b95af624a49c8e735cbcf9edfbd6ab77
dmytrome <dmytrom@migfin.com>
2015-09-16 13:39:04 +0300

    AC-24 Создать UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\RadBundle\KnpRadBundle(),
+
             new Araneum\Bundle\MainBundle\AraneumMainBundle(),
+            new Araneum\Bundle\MainBundle\AraneumUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
