ac2b2ebc57e2cbcb780412061b2bb58179ec9156
Alexandr Litvin <lit8lex@gmail.com>
2015-09-17 12:44:51 +0300

    AC-35 Создать архитектуру Connection

--- src/Araneum/Bundle/MainBundle/Entity/Connection.php
+++ src/Araneum/Bundle/MainBundle/Entity/Connection.php
@@ -30,12 +30,12 @@ class Connection
     protected $type;
 
     /**
-     * @ORM\Column(type="string", name="name", unique="true" length=100)
+     * @ORM\Column(type="string", name="name", unique=true, length=100)
      */
     protected $name;
 
     /**
-     * @ORM\Column(type="string", name="host" length=100)
+     * @ORM\Column(type="string", name="host", length=100)
      */
     protected $host;
 
@@ -45,7 +45,7 @@ class Connection
     protected $port;
 
     /**
-     * @ORM\Column(type="string", name="user_name" length=100)
+     * @ORM\Column(type="string", name="user_name", length=100)
      */
     protected $user_name;
 
@@ -68,4 +68,221 @@ class Connection
      * @ORM\Column(type="datetime", name="updated_at")
      */
     protected $updated_at;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     * @return Connection
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Connection
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set host
+     *
+     * @param string $host
+     * @return Connection
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    /**
+     * Get host
+     *
+     * @return string 
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Set port
+     *
+     * @param integer $port
+     * @return Connection
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+
+        return $this;
+    }
+
+    /**
+     * Get port
+     *
+     * @return integer 
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Set user_name
+     *
+     * @param string $userName
+     * @return Connection
+     */
+    public function setUserName($userName)
+    {
+        $this->user_name = $userName;
+
+        return $this;
+    }
+
+    /**
+     * Get user_name
+     *
+     * @return string 
+     */
+    public function getUserName()
+    {
+        return $this->user_name;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Connection
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return Connection
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return Connection
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param \DateTime $updatedAt
+     * @return Connection
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
 }
