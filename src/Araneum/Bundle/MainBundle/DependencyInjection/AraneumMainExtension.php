dacb77f0002e69bcd267a3736ff67ff6e2e6f1bc
Artem Morozov <artemm@migfin.com>
2015-09-21 11:20:50 +0300

    Draft AC-26

--- src/Araneum/Bundle/MainBundle/DependencyInjection/AraneumMainExtension.php
+++ src/Araneum/Bundle/MainBundle/DependencyInjection/AraneumMainExtension.php
@@ -24,5 +24,8 @@ class AraneumMainExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $loaderYml = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loaderYml->load('admin.yml');
     }
 }
