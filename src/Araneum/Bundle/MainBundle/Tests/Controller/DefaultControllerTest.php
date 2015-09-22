eee4da6697f2ddca913477b27ca15b7e80ff905d
Maksymc <maksymc@migfin.com>
2015-09-22 17:07:59 +0300

    AC-55 Удалить неиспользуемые файлы

--- src/Araneum/Bundle/MainBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Araneum/Bundle/MainBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,10 +1,18 @@
 <?php
 
-//namespace Araneum\Bundle\MainBundle\Tests\Controller;
-//
-//use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-//
-//class DefaultControllerTest extends WebTestCase
-//{
-//
-//}
+namespace Araneum\Bundle\MainBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("test")')->count() > 0);
+    }
+}
