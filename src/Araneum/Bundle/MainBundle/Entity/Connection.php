8802a04c603d646fc4a13ed14276d16f41977181
Alexandr Litvin <lit8lex@gmail.com>
2015-09-22 16:47:01 +0300

    AC-34 Админ панель для Connection. Add test connection button. Add validation. Check config.

--- src/Araneum/Bundle/MainBundle/Entity/Connection.php
+++ src/Araneum/Bundle/MainBundle/Entity/Connection.php
@@ -4,20 +4,24 @@ namespace Araneum\Bundle\MainBundle\Entity;
 
 use Araneum\Base\EntityTrait\DateTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Class Connection
+ *
  * @ORM\HasLifecycleCallbacks
  * @ORM\Table(name="araneum_connections")
  * @ORM\Entity(repositoryClass="Araneum\Bundle\MainBundle\Repository\ConnectionRepository")
+ * @UniqueEntity(fields="name")
  * @package Araneum\Bundle\MainBundle\Entity
  */
 class Connection
 {
     use DateTrait;
 
-    const CONN_DB = 1;
-    const CONN_HOST = 2;
+    const CONN_DB     = 1;
+    const CONN_HOST   = 2;
     const CONN_TO_STR = 'Create';
 
     /**
@@ -34,26 +38,31 @@ class Connection
 
     /**
      * @ORM\Column(type="string", name="name", unique=true, length=100)
+     * @Assert\Length(min=3, max=100)
      */
     protected $name;
 
     /**
      * @ORM\Column(type="string", name="host", length=100)
+     * @Assert\Length(min=3, max=100)
      */
     protected $host;
 
     /**
-     * @ORM\Column(type="integer", name="port", length=100, nullable=true)
+     * @ORM\Column(type="integer", name="port", length=8, nullable=true)
+     * @Assert\Length(min=2, max=8)
      */
     protected $port;
 
     /**
      * @ORM\Column(type="string", name="user_name", length=100, nullable=true)
+     * @Assert\Length(min=3, max=100)
      */
     protected $userName;
 
     /**
      * @ORM\Column(type="string", name="password", length=100)
+     * @Assert\Length(min=6, max=100)
      */
     protected $password;
 
@@ -65,7 +74,7 @@ class Connection
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
@@ -88,7 +97,7 @@ class Connection
     /**
      * Get type
      *
-     * @return integer 
+     * @return integer
      */
     public function getType()
     {
@@ -111,7 +120,7 @@ class Connection
     /**
      * Get name
      *
-     * @return string 
+     * @return string
      */
     public function getName()
     {
@@ -134,7 +143,7 @@ class Connection
     /**
      * Get host
      *
-     * @return string 
+     * @return string
      */
     public function getHost()
     {
@@ -157,7 +166,7 @@ class Connection
     /**
      * Get port
      *
-     * @return integer 
+     * @return integer
      */
     public function getPort()
     {
@@ -180,7 +189,7 @@ class Connection
     /**
      * Get userName
      *
-     * @return string 
+     * @return string
      */
     public function getUserName()
     {
@@ -203,7 +212,7 @@ class Connection
     /**
      * Get password
      *
-     * @return string 
+     * @return string
      */
     public function getPassword()
     {
@@ -226,7 +235,7 @@ class Connection
     /**
      * Get enabled
      *
-     * @return boolean 
+     * @return boolean
      */
     public function isEnabled()
     {
@@ -240,8 +249,8 @@ class Connection
      */
     public function __toString()
     {
-        if(!empty($this->getName())) {
-            return $this->getName()." (".$this->getHost().")";
+        if (!empty($this->getName())) {
+            return $this->getName() . " (" . $this->getHost() . ")";
         } else {
             return self::CONN_TO_STR;
         }
