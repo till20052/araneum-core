777ddfb6998d507b1711ef2cd1ad3aef1c93587c
dmytrome <dmytrom@migfin.com>
2015-09-23 12:30:10 +0300

    draft AC-33 Админ панель для User

---
+++ src/Araneum/Bundle/UserBundle/Admin/UserAdmin.php
@@ -0,0 +1,91 @@
+<?php
+namespace Araneum\Bundle\UserBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class UserAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('email', null, ['label' => 'email'])
+            ->add('username', null, ['label' => 'username'])
+            ->add('fullName', null, ['label' => 'fullName'])
+            ->add('enabled', null, ['label' => 'enabled'])
+            ->add('roles', null, ['label' => 'roles']);
+
+        if ($this->getSubject()->getId() === null) {
+            $formMapper->add('plainPassword', 'text', ['label' => 'password']);
+        }
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('fullName', null, ['label' => 'fullName'])
+            ->add('email', null, ['label' => 'email'])
+            ->add('enabled', null, ['label' => 'enabled'])
+            ->add(
+                'createdAt',
+                'doctrine_orm_date_range',
+                [
+                    'field_type' => 'sonata_type_date_range_picker',
+                    'label' => 'created_at',
+                ],
+                null,
+                ['format' => 'MM/dd/y']
+            );
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('email', null, ['label' => 'email'])
+            ->add('fullName', null, ['label' => 'fullName'])
+            ->add(
+                'enabled',
+                null,
+                [
+                    'editable' => true,
+                    'label' => 'enabled'
+                ]
+            )
+            ->add('lastLogin', null, ['label' => 'last_login'])
+            ->add('roles', null, ['label' => 'roles'])
+            ->add('createdAt', null,
+                [
+                    'format' => 'm/d/Y',
+                    'label' => 'created_at'
+                ]
+            )
+            ->add('updatedAt', null,
+                [
+                    'format' => 'm/d/Y',
+                    'label' => 'updated_at'
+                ]
+            )
+            ->add(
+                '_action',
+                'actions',
+                [
+                    'actions' => [
+                        'edit' => [],
+                        'delete' => [],
+                        'activateUser' => [
+                            'template' => 'AraneumUserBundle:AdminAction:activateUser.html.twig'
+                        ],
+                        'recoveryPassword' => [
+                            'template' => 'AraneumUserBundle:AdminAction:recoveryPassword.html.twig'
+                        ],
+                    ],
+                    'label' => 'actions'
+                ]
+            );
+    }
+}
---
+++ src/Araneum/Bundle/UserBundle/Controller/CRUDController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Araneum\Bundle\UserBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as AdminController;
+
+class CRUDController extends AdminController
+{
+    public function activateUserAction()
+    {
+        // TODO: add you implementation here
+    }
+
+    public function recoverPasswordAction()
+    {
+        // TODO: add you implementation here
+    }
+}
