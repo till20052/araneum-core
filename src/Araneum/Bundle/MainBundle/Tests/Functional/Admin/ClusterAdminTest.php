60830d230b39bc013b2f0c7598dd44052f8c6870
andreyp <andreyp@migfin.com>
2015-09-25 18:41:37 +0300

    AC-39 Админ панель для Cluster

--- src/Araneum/Bundle/MainBundle/Tests/Functional/Admin/ClusterAdminTest.php
+++ src/Araneum/Bundle/MainBundle/Tests/Functional/Admin/ClusterAdminTest.php
@@ -9,18 +9,32 @@
 namespace Araneum\Bundle\MainBundle\Tests\Functional\Admin;
 
 use Araneum\Base\Tests\Controller\BaseController;
-use Symfony\Component\HttpFoundation\Response;
+use Araneum\Bundle\MainBundle\Entity\Cluster;
 use Araneum\Bundle\MainBundle\Tests\Functional\Utils\Data;
-
+use Symfony;
 
 
 class ClusterAdminTest extends BaseController
 {
 
     private $prefix;
-    const FILTER = ['name' => 'functionalTestConnection'];
+    static $client;
+    static $connId;
+    const FILTER_CONNECTION = ['name' => 'functionalTestConnection'];
+    const FILTER_CLUSTER_NAME = ['name' => 'clusterTestName'];
     const TYPE_MULTIPLE = 2;
     const STATUS_ONLINE = 1;
+
+
+    /**
+     * Setup befor class
+     */
+    public static function setUpBeforeClass(){
+        $client = static::createClient();
+        Data\ManageTestEntities::deleteClusterByName($client->getContainer()->get('doctrine.orm.default_entity_manager'), self::FILTER_CLUSTER_NAME);
+        $conn = Data\ManageTestEntities::CreateConnection($client->getContainer()->get('doctrine.orm.default_entity_manager'), self::FILTER_CONNECTION);
+        self::$connId = $conn->getId();
+    }
 
     /**
      * @beforeClass
@@ -33,33 +47,75 @@ class ClusterAdminTest extends BaseController
     }
 
     /**
-     * Test for create cluster
-     *
      * @dataProvider saveProvider
      * @param $name
-     * @param $host
      * @param $type
+     * @param $status
      * @param $enabled
-     * @param $type
      * @param $expects
      * @runInSeparateProcess
      */
-    public function testCreateAction($name, $host, $type, $enabled, $type, $expects){
+    public function testCreateAction($name, $type, $status, $enabled, $expects){
+
         $client = $this->createAdminAuthorizedClient();
 
-        $connection = Data\CreateTestEntities::CreateConnection($client->getContainer()->get('doctrine.orm.default_entity_manager'), self::FILTER);
+        $router = $client->getContainer()->get('router');
 
-        $crawler = $client->request('GET', '/en/admin/araneum/main/cluster/create');
+        $crawler = $client->request('GET', $router->generate('admin_araneum_main_cluster_create'));
 
-        $form = $crawler->selectButton('btn_create_and_list')->form();
+        $form = $crawler->selectButton('btn_create_and_edit')->form();
 
         $this->getFormPrefix($form);
 
         $prefix = $this->prefix;
 
         $scrawler = $client->submit($form, [
-
+                $prefix.'[name]' => $name['name'],
+                $prefix.'[host]' => self::$connId,
+                $prefix.'[type]' => $type,
+                $prefix.'[status]' =>$status,
+                $prefix.'[enabled]' => $enabled,
         ]);
+        $this->assertEquals(count($crawler->filter('.alert-danger'))==0, $expects);
+
+        $client = static::createClient();
+        Data\ManageTestEntities::deleteClusterByName($client->getContainer()->get('doctrine.orm.default_entity_manager'), self::FILTER_CLUSTER_NAME);
+    }
+
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testUpdateAction(){
+
+        $client = static::createClient();
+
+        $cluster = Data\ManageTestEntities::CreateCluster($client->getContainer()->get('doctrine.orm.default_entity_manager'), self::FILTER_CLUSTER_NAME, self::FILTER_CONNECTION);
+
+        $clusterId = $cluster->getId();
+
+        $client = $this->createAdminAuthorizedClient();
+
+        $router = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $router->generate('admin_araneum_main_cluster_edit', array('id' => $clusterId)));
+
+        $form = $crawler->selectButton('btn_update_and_edit')->form();
+
+        $this->getFormPrefix($form);
+
+        $prefix = $this->prefix;
+
+        $name = self::FILTER_CLUSTER_NAME;
+
+        $scrawler = $client->submit($form, [
+            $prefix.'[name]' => $name['name'],
+            $prefix.'[type]' => Cluster::TYPE_SINGLE,
+            $prefix.'[status]' =>Cluster::STATUS_OFFLINE,
+            $prefix.'[enabled]' => false,
+        ]);
+        $this->assertEquals(count($crawler->filter('.alert-danger'))==0, true);
+
     }
 
 
@@ -68,49 +124,48 @@ class ClusterAdminTest extends BaseController
      *
      * @return array
      */
-    protected function saveProvider(){
+    public function saveProvider(){
         return [
             [
-                'testName',
-                'testHost',
-                2222222,
-                true,
-                self::STATUS_ONLINE,
-                false
-            ],
-            [
-                'testName',
-                'testHost',
-                self::TYPE_MULTIPLE,
-                '3',
-                self::STATUS_ONLINE,
-                false
-            ],
-            [
-                'testName',
-                'testHost',
-                self::TYPE_MULTIPLE,
-                true,
-                '44',
-                false
-            ],
-            [
-                'testName',
-                'testHost',
-                self::TYPE_MULTIPLE,
+                self::FILTER_CLUSTER_NAME,
+                2,
                 true,
                 self::STATUS_ONLINE,
                 true
             ],
             [
-                'testName',
-                'testHost',
+                self::FILTER_CLUSTER_NAME,
+                self::TYPE_MULTIPLE,
+                '1',
+                self::STATUS_ONLINE,
+                true
+            ],
+            [
+                self::FILTER_CLUSTER_NAME,
+                self::TYPE_MULTIPLE,
+                true,
+                '1',
+                true
+            ],
+            [
+                self::FILTER_CLUSTER_NAME,
                 self::TYPE_MULTIPLE,
                 true,
                 self::STATUS_ONLINE,
-                false
-            ],
+                true
+            ]
         ];
 
+    }
+
+    /**
+     * Tear down After class
+     *
+     * clean test data Cluster
+     */
+    public static function tearDownAfterClass()
+    {
+        $client = static::createClient();
+        Data\ManageTestEntities::deleteClusterByName($client->getContainer()->get('doctrine.orm.default_entity_manager'), self::FILTER_CLUSTER_NAME);
     }
 }
