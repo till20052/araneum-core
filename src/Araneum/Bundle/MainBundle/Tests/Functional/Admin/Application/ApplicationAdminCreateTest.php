6b348e25331c1a5857a544756f617b1b75a4e967
Artem Morozov <artemm@migfin.com>
2015-09-30 16:30:13 +0300

    AC-42 Админ панель для Application

---
+++ src/Araneum/Bundle/MainBundle/Tests/Functional/Admin/Application/ApplicationAdminCreateTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artemm
+ * Date: 30.09.15
+ * Time: 16:27
+ */
+
+namespace Araneum\Bundle\MainBundle\Tests\Functional\Admin\Application;
+
+
+class ApplicationAdminTest
+{
+
+}
