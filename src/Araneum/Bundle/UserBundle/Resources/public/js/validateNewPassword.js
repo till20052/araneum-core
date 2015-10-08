3b1cd146004b6a4de8dc5670f825c56542ad30eb
nbfx <xdr.noobfix@gmail.com>
2015-10-08 10:15:46 +0300

    AC-76 Настроить и кастомизировать сброс пароля пользователя / Need to configure and customize the...

---
+++ src/Araneum/Bundle/UserBundle/Resources/public/js/validateNewPassword.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#new_password_form').validate({
+        rules: {
+            'fos_user_resetting_form[new][first]': {
+                required: true,
+                minlength: 3
+            },
+            'fos_user_resetting_form[new][second]': {
+                required: true,
+                minlength: 3
+            }
+        },
+        errorElement: 'p',
+        errorLabelContainer: '#msgbox',
+        messages: {
+            'fos_user_resetting_form[new][first]': "Password must contain 3 characters minimum",
+            'fos_user_resetting_form[new][second]': "Password repeat must contain 3 characters minimum"
+        }
+    });
+
+    $('#msgbox').hide();
+});
