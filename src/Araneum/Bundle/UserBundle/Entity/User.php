3d24758ec8ec268e1d9a0281d27d02c5d4b5df8a
nbfx <xdr.noobfix@gmail.com>
2015-10-13 09:37:15 +0300

    draft AC-56 Изменить поле roles в сущности User

--- src/Araneum/Bundle/UserBundle/Entity/User.php
+++ src/Araneum/Bundle/UserBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace Araneum\Bundle\UserBundle\Entity;
 use Araneum\Base\EntityTrait\DateTrait;
 use \FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * User class
@@ -35,6 +36,22 @@ class User extends BaseUser
     private $fullName;
 
     /**
+     * @var ArrayCollection
+     * @ORM\ManyToMany(targetEntity="Role", cascade={"detach", "persist"}, inversedBy="users")
+     * @ORM\JoinTable(name="araneum_user_role")
+     */
+    protected $roles;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->roles = new ArrayCollection();
+    }
+
+    /**
      * @return string
      */
     public function getFullName()
@@ -52,5 +69,89 @@ class User extends BaseUser
         $this->fullName = $fullName;
 
         return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        $roles = [];
+        foreach ($this->roles->toArray() as $k => $role) {
+
+            $roles[$role->getId()] = $role->getName();
+        }
+
+        return $roles;
+    }
+
+    /**
+     * @param array $roles
+     *
+     * @return User
+     */
+    public function setRoles(array $roles)
+    {
+
+        //TODO  override
+        $this->roles = new ArrayCollection($roles);
+
+        return $this;
+    }
+
+    public function getRolesCollection()
+    {
+        return $this->roles->first();
+    }
+
+    /**
+     * @param string $role
+     * @return $this
+     */
+    public function removeRole($role)
+    {
+//        die(var_dump($role));
+
+        //TODO исправь!!!
+        $role = $this->roles->filter(
+
+            function (Role $r) use ($role) {
+                if ($role instanceof Role) {
+                    return $r->getRole() === $role->getRole();
+                } else {
+                    return $r->getRole() === strtoupper($role);
+                }
+            }
+
+        )->first();
+        if ($role) {
+            $this->roles->removeElement($role);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $role
+     * @return $this
+     */
+    public function addRole($role)
+    {
+        if (!$role instanceof Role) {
+            $role = new Role($role);
+        }
+
+        $this->roles->add($role);
+
+        return $this;
+    }
+
+    /**
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole($role)
+    {
+        return in_array(strtoupper($role), $this->getRoles()->toArray(), true);
     }
 }
---
+++ src/Araneum/Bundle/UserBundle/Repository/RoleRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Araneum\Bundle\UserBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class RoleRepository extends EntityRepository
+{
+}
