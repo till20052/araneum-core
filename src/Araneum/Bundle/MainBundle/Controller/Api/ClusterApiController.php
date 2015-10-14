919d0150ea5a718a9510fbeff802977ba8fe4f8d
Artem Morozov <artemm@migfin.com>
2015-10-14 12:57:28 +0300

    AC-119 Create API (GET) method which will return list of cluster projects

--- src/Araneum/Bundle/MainBundle/Controller/Api/ClusterApiController.php
+++ src/Araneum/Bundle/MainBundle/Controller/Api/ClusterApiController.php
@@ -2,7 +2,46 @@
 
 namespace Araneum\Bundle\MainBundle\Controller\Api;
 
-class ClusterApiController
-{
+use FOS\RestBundle\Controller\FOSRestController;
 
+class ClusterApiController extends FOSRestController
+{
+	/**
+	 * Get Application config by apiKey
+	 *
+	 * @ApiDoc(
+	 *   resource = "Cluster",
+	 *   section = "MainBundle",
+	 *   description = "Gets a Application config for a given key",
+	 *   output = "Araneum\Bundle\MainBundle\Entity\Application",
+	 *   statusCodes = {
+	 *      200 = "Returned when successful",
+	 *      403 = "Returned when authorization is failed",
+	 *      404 = "Returned when Application not found"
+	 *   },
+	 *   requirements = {
+	 *      {
+	 *          "name" = "_format",
+	 *          "dataType" = "json",
+	 *          "description" = "Output format must be json"
+	 *      }
+	 *   },
+	 *   parameters={
+	 *      {"name"="apiKey", "dataType"="string", "required"=true, "description"="apiKey"}
+	 *   },
+	 *   tags={"ApplicationApi"}
+	 * )
+	 *
+	 * @Route("/application/config/{apiKey}", name="araneum_main_api_application")
+	 * @Method({"GET"})
+	 *
+	 * @Rest\View(templateVar="application")
+	 *
+	 * @param string $apiKey The application apiKey
+	 * @return array
+	 */
+	public function applicationsConfigsList()
+	{
+
+	}
 }
---
+++ src/Araneum/Bundle/MainBundle/Handler/ClusterHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artemm
+ * Date: 14.10.15
+ * Time: 12:57
+ */
+
+namespace Araneum\Bundle\MainBundle\Handler;
+
+
+class ClusterHandler
+{
+
+}
