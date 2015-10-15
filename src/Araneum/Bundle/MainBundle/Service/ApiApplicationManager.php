d2c0b83deaeec1d6f3c141b56c5ee7a69a2e1d5c
andreyp <andreyp@migfin.com>
2015-10-15 10:23:45 +0300

    AC-146 Create stub for RemoteApplicationManager

--- src/Araneum/Bundle/MainBundle/Service/ApiApplicationManager.php
+++ src/Araneum/Bundle/MainBundle/Service/ApiApplicationManager.php
@@ -9,11 +9,21 @@ class ApiApplicationManager
 {
     private $repository;
 
+    /**
+     * Constructor
+     *
+     * @param $doctrine
+     */
     public function __construct($doctrine)
     {
         $this->repository = $doctrine->getRepository('AraneumMainBundle:Application');
     }
 
+    /**
+     * Create application
+     *
+     * @return Application
+     */
     public function createApplication()
     {
         $entity = $this->repository->findOneByName(ApplicationFixtures::TEST_APP_NAME);
@@ -21,6 +31,12 @@ class ApiApplicationManager
         return $entity;
     }
 
+    /**
+     * Get application
+     *
+     * @param $appKey
+     * @return Application
+     */
     public function getApplication($appKey)
     {
         $entity = $this->repository->findOneBy(['app_key' => $appKey]);
@@ -28,6 +44,12 @@ class ApiApplicationManager
         return $entity;
     }
 
+    /**
+     * Update application
+     *
+     * @param $appKey
+     * @return Application
+     */
     public function updateApplication($appKey)
     {
         $entity = $this->repository->findOneBy(['app_key' => $appKey]);
@@ -35,13 +57,26 @@ class ApiApplicationManager
         return $entity;
     }
 
+    /**
+     * Delete application
+     *
+     * @param $appKey
+     * @return bool
+     */
     public function deleteApplication($appKey)
     {
         return true;
     }
 
+    /**
+     * Get cluster
+     *
+     * @param $appKey
+     * @return mixed
+     */
     public function getClusterByAppKey($appKey)
     {
+        return null;
     }
 
 }
