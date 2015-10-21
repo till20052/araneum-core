156ac863d67bfab93304d03b3d48e39f1b5a62db
Artem Morozov <artemm@migfin.com>
2015-10-21 10:36:53 +0300

    AC-41 Service for check status of Aplication

--- src/Araneum/Bundle/MainBundle/Controller/AdminConnectionController.php
+++ src/Araneum/Bundle/MainBundle/Controller/AdminConnectionController.php
@@ -3,27 +3,21 @@
 namespace Araneum\Bundle\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class AdminConnectionController extends Controller
 {
+
     /**
-     * Check Connection Status State
+     * Test connection
      *
      * @param $id
-     * @Route("/check_status_state/{id}", name="araneum_main_admin_connection_check_status_state")
-     *
-     * @return JsonResponse
+     * @return Response
+     * @Route("/testConnection/{id}", name="araneum_main_admin_connection_testConnection")
      */
-    public function checkStatusStateAction($id)
+    public function testConnectionAction($id)
     {
-		$this->container->get('araneum.main.application.checker');
-
-        return new JsonResponse(
-	        [
-	            'success' => 1
-            ]
-        );
+        return new Response();
     }
 }
