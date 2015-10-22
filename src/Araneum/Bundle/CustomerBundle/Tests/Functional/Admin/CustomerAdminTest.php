956b43aa21662b56af625a10b6eaa52bbb541ae7
andreyp <andreyp@migfin.com>
2015-10-22 19:17:47 +0300

    AC-145 Need to remove in section Cusomer, button [Add new]

--- src/Araneum/Bundle/CustomerBundle/Tests/Functional/Admin/CustomerAdminTest.php
+++ src/Araneum/Bundle/CustomerBundle/Tests/Functional/Admin/CustomerAdminTest.php
@@ -5,53 +5,98 @@ namespace Araneum\Bundle\CustomerBundle\Tests\Functional\Admin;
 use Araneum\Base\Tests\Controller\BaseAdminController;
 use Araneum\Base\Tests\Fixtures\Customer\CustomerFixtures;
 use Araneum\Base\Tests\Fixtures\Main\ApplicationFixtures;
+use Araneum\Base\Tests\Controller\BaseController;
 
-class CustomerAdminTest extends BaseAdminController
+class CustomerAdminTest extends BaseController
 {
 	const TEST_CUSTOMER_EMAIL = 'TestCustomerEmail';
 
-	protected $createRoute = 'admin_araneum_customer_customer_create';
-	protected $updateRoute = 'admin_araneum_customer_customer_edit';
-	protected $deleteRoute = 'admin_araneum_customer_customer_delete';
-	protected $listRoute = 'admin_araneum_customer_customer_list';
+	/**
+	 * Test is create action is disabled
+	 *
+	 * @runInSeparateProcess
+	 */
+	public function testDisableCreate()
+	{
+		$client = $this->createAdminAuthorizedClient();
+
+		$crawler = $client->request(
+			'GET',
+			'/en/admin/araneum/customer/customer/create'
+		);
+
+		$this->assertFalse($client->getResponse()->isSuccessful());
+	}
 
 	/**
-	 * Set of arguments for testCreate method
+	 * Test is edit action is disabled
 	 *
-	 * @return array
+	 * @runInSeparateProcess
 	 */
-	public function createDataSource()
+	public function testDisableEdit()
 	{
-		$client = static::createClient();
+		$client = $this->createAdminAuthorizedClient();
 
-		$manager = $client->getContainer()->get('doctrine.orm.entity_manager');
+		$customer = $client->getContainer()
+			->get('doctrine.orm.entity_manager')
+			->getRepository('AraneumCustomerBundle:Customer')
+			->findOneByEmail(CustomerFixtures::TEST_EMAIL);
 
-		$application = $manager
-			->getRepository('AraneumMainBundle:Application')
-			->findOneByName(ApplicationFixtures::TEST_APP_NAME);
+		$crawler = $client->request(
+			'GET',
+			'/en/admin/araneum/customer/customer/' . $customer->getId() . '/edit'
+		);
 
-		return [
-			'Check simple creation' => [
-				[
-					'application' => $application->getId(),
-					'firstName' => 'TestCustomerFirstName',
-					'lastName' => 'TestCustomerLastName',
-					'country' => 'TestCustomerCountry',
-					'email' => self::TEST_CUSTOMER_EMAIL . '@' . md5(self::TEST_CUSTOMER_EMAIL) . '.com',
-					'phone' => '+380995554455',
-					'callback' => true,
-				],
-				true
-			],
-			'Check customer unique email' => [
-				[
-					'application' => $application->getId(),
-					'email' => CustomerFixtures::TEST_EMAIL
-				],
-				false
-			]
-		];
+		$this->assertFalse($client->getResponse()->isSuccessful());
 	}
+
+	/**
+	 * Test is delete action is disabled
+	 *
+	 * @runInSeparateProcess
+	 */
+	public function testDisableDelete()
+	{
+		$client = $this->createAdminAuthorizedClient();
+
+		$customer = $client->getContainer()
+			->get('doctrine.orm.entity_manager')
+			->getRepository('AraneumCustomerBundle:Customer')
+			->findOneByEmail(CustomerFixtures::TEST_EMAIL);
+
+		$crawler = $client->request(
+			'GET',
+			'/en/admin/araneum/customer/customer/' . $customer->getId() . '/delete'
+		);
+
+		$this->assertFalse($client->getResponse()->isSuccessful());
+	}
+
+	/**
+	 * Show test
+	 *
+	 * @runInSeparateProcess
+	 */
+	public function testShow()
+	{
+		$client = $this->createAdminAuthorizedClient();
+
+		$customer = $client->getContainer()
+			->get('doctrine.orm.entity_manager')
+			->getRepository('AraneumCustomerBundle:Customer')
+			->findOneByEmail(CustomerFixtures::TEST_EMAIL);
+
+		$crawler = $client->request(
+			'GET',
+			$client->getContainer()->get('router')->generate(
+				'admin_araneum_customer_customer_show',
+				['id' => $customer->getId()]
+			)
+		);
+
+		$this->assertTrue($client->getResponse()->isSuccessful());
+	}
+
 
 	/**
 	 * Set of arguments for testFilter method
@@ -110,71 +155,5 @@ class CustomerAdminTest extends BaseAdminController
 				$anotherCustomer
 			]
 		];
-	}
-
-	/**
-	 * Set of arguments for testUpdate method
-	 *
-	 * @return array
-	 */
-	public function updateDataSource()
-	{
-		$client = static::createClient();
-
-		$manager = $client->getContainer()->get('doctrine.orm.entity_manager');
-
-		$customer = $manager
-			->getRepository('AraneumCustomerBundle:Customer')
-			->findOneByEmail(CustomerFixtures::TEST_2_EMAIL);
-
-		return [
-			'Check simple modification' => [
-				[
-					'firstName' => 'TestCustomer2UpdateFirstName',
-					'lastName' => 'TestCustomer2UpdateLastName',
-					'country' => 'TestCustomer2UpdateCountry',
-					'email' => self::TEST_CUSTOMER_EMAIL . '@' . md5(microtime(true)) . '.com',
-					'phone' => '+380667754444',
-					'callback' => ! CustomerFixtures::TEST_2_CALLBACK
-				],
-				true,
-				$customer
-			],
-			'Check updating email when customer with this email already exists' => [
-				[
-					'email' => CustomerFixtures::TEST_EMAIL,
-				],
-				false,
-				$customer
-			],
-			'Set first values of temp application' => [
-				[
-					'firstName' => CustomerFixtures::TEST_2_FIRST_NAME,
-					'lastName' => CustomerFixtures::TEST_2_LAST_NAME,
-					'country' => CustomerFixtures::TEST_2_COUNTRY,
-					'email' => CustomerFixtures::TEST_2_EMAIL,
-					'phone' => CustomerFixtures::TEST_2_PHONE,
-					'callback' => CustomerFixtures::TEST_2_CALLBACK
-				],
-				true,
-				$customer
-			]
-		];
-	}
-
-	/**
-	 * Return entity for testDelete method
-	 *
-	 * @return mixed
-	 */
-	public function deleteDataSource()
-	{
-		$client = static::createClient();
-
-		return $client
-			->getContainer()
-			->get('doctrine.orm.entity_manager')
-			->getRepository('AraneumCustomerBundle:Customer')
-			->findOneByEmail(self::TEST_CUSTOMER_EMAIL . '@' . md5(self::TEST_CUSTOMER_EMAIL) . '.com');
 	}
 }
