e6a9089d6058eaa1e4ae6ed5f167c61d612f6d14
andreyp <andreyp@migfin.com>
2015-10-22 18:49:34 +0300

    AC-151 In the section "Cluster" and "Connection in column "Name", names can be changed to empty f...

--- src/Araneum/Bundle/MainBundle/Tests/Functional/RecursivePagesTest.php
+++ src/Araneum/Bundle/MainBundle/Tests/Functional/RecursivePagesTest.php
@@ -116,8 +116,8 @@ class RecursivePagesTest extends BaseController
 
 		foreach($this->register as $url => $data)
 		{
-			$this->assertGreaterThanOrEqual(200, $data['is_successful'], $data['status_code'] . "\t" . $url . "\n");
-			$this->assertLessThanOrEqual(400, $data['is_successful'], $data['status_code'] . "\t" . $url . "\n");
+			$this->assertGreaterThanOrEqual(200, $data['status_code'], $data['status_code'] . "\t" . $url . "\n");
+			$this->assertLessThanOrEqual(400, $data['status_code'], $data['status_code'] . "\t" . $url . "\n");
         }
 	}
 }
