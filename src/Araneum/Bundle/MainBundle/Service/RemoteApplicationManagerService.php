1ac84f0e011c10df00584b9a675d18b869259c0f
andreyp <andreyp@migfin.com>
2015-10-26 15:01:46 +0200

    AC-120 Create service to control application configs

--- src/Araneum/Bundle/MainBundle/Service/RemoteApplicationManagerService.php
+++ src/Araneum/Bundle/MainBundle/Service/RemoteApplicationManagerService.php
@@ -2,46 +2,89 @@
 
 namespace Araneum\Bundle\MainBundle\Service;
 
+use Doctrine\ORM\EntityManager;
+use Guzzle\Service\Client;
+use Symfony\Component\HttpFoundation\Response;
+use Guzzle\Http\Exception\CurlException;
+use Guzzle\Http\Message\Response as GuzzleResponse;
+
 class RemoteApplicationManagerService
 {
-	/**
-	 * Get application data from cluster
-	 */
-	public function get()
-	{
 
-	}
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
 
-	/**
-	 * Create application on cluster
-	 *
-	 * @param string $appKey
-	 * @return bool
-	 */
-	public function create($appKey)
-	{
-		return true;
-	}
+    /**
+     * @var Client
+     */
+    private $client;
 
-	/**
-	 * Update application config in cluster
-	 *
-	 * @param string $appKey
-	 * @return bool
-	 */
-	public function update($appKey)
-	{
-		return true;
-	}
+    /**
+     * Remote application handler constructor
+     *
+     * @param EntityManager $entityManager
+     * @param Client        $client
+     */
+    public function __construct(EntityManager $entityManager, Client $client)
+    {
+        $this->entityManager = $entityManager;
+        $this->client = $client;
+    }
 
-	/**
-	 * Remove application from cluster
-	 *
-	 * @param string $appKey
-	 * @return bool
-	 */
-	public function remove($appKey)
-	{
-		return true;
-	}
+    /**
+     * Get application data from cluster
+     */
+    public function get($clusterId)
+    {
+        $repository = $this->entityManager->getRepository('AraneumMainBundle:Application');
+        $connections = $repository->findByConnectionId($clusterId);
+
+        try {
+            $response = $this->client
+                ->get('http://' . $connections->getDomain())
+                ->send();
+
+            $status = in_array(
+                $response->getStatusCode(),
+                range(Response::HTTP_OK, Response::HTTP_MULTI_STATUS) + [Response::HTTP_IM_USED]
+            );
+        } catch (CurlException $e) {
+            return $e;
+        }
+    }
+
+    /**
+     * Create application on cluster
+     *
+     * @param string $appKey
+     * @return bool
+     */
+    public function create($appKey)
+    {
+        return true;
+    }
+
+    /**
+     * Update application config in cluster
+     *
+     * @param string $appKey
+     * @return bool
+     */
+    public function update($appKey)
+    {
+        return true;
+    }
+
+    /**
+     * Remove application from cluster
+     *
+     * @param string $appKey
+     * @return bool
+     */
+    public function remove($appKey)
+    {
+        return true;
+    }
 }
