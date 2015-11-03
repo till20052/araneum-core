f2349ff86b94c8f373de01316f8d78a922e8f91a
andreyp <andreyp@migfin.com>
2015-11-03 12:13:44 +0200

    AC-193 Can change connection to an empty field "Port" and "User name"

--- src/Araneum/Bundle/MainBundle/Controller/ClusterApiController.php
+++ src/Araneum/Bundle/MainBundle/Controller/ClusterApiController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class ClusterApiController extends FOSRestController
 {
 	/**
-	 * Get configurations list of applications which cluster contains
+	 * Get configurations list of applications which cluster from core
 	 *
 	 * @ApiDoc(
 	 *   resource = "Cluster",
@@ -45,7 +45,7 @@ class ClusterApiController extends FOSRestController
 	 * @param int $clusterId The cluster id
 	 * @return array
 	 */
-	public function applicationsConfigsListAction($clusterId)
+	public function applicationsCoreConfigsListAction($clusterId)
 	{
 		$list = $this->container
 			->get('araneum.main.cluster.api_handler')
@@ -56,5 +56,49 @@ class ClusterApiController extends FOSRestController
 		}
 
 		return $list;
+	}
+
+	/**
+	 * Get configurations list of applications which cluster from cluster
+	 *
+	 * @ApiDoc(
+	 *   resource = "Cluster",
+	 *   section = "MainBundle",
+	 *   description = "Get configurations list of applications which cluster contains",
+	 *   statusCodes = {
+	 *      200 = "Returned when successful",
+	 *      403 = "Returned when authorization is failed",
+	 *      404 = "Returned when Cluster not found"
+	 *   },
+	 *   requirements = {
+	 *      {
+	 *          "name" = "_format",
+	 *          "dataType" = "json",
+	 *          "description" = "Output format must be json"
+	 *      }
+	 *   },
+	 *   parameters={
+	 *      {"name"="clusterId", "dataType"="int", "required"=true, "description"="The cluster id"}
+	 *   },
+	 *   tags={"ClusterApi"}
+	 * )
+	 *
+	 * @Rest\Get(
+	 *      "/api/getAppConfig/{clusterId}",
+	 *      name="araneum_main_api_cluster_remote_application_config",
+	 *      defaults={"_format"="json", "_locale"="en"}
+	 * )
+	 *
+	 * @Rest\View()
+	 *
+	 * @param int $clusterId The cluster id
+	 * @return array
+	 */
+	public function remoteApplicationGetData($clusterId)
+	{
+		$connection = $this->container
+			->get('doctrine.orm.entity_manager')
+			->getRepository('AraneumMainBundle:Connection')
+			->getHostByClusterId($clusterId);
 	}
 }
