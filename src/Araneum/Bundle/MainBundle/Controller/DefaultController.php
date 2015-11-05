9c6947a6acb4341fecc3e7e92e3b6c74abbcbe21
andreyp <andreyp@migfin.com>
2015-11-05 14:20:44 +0200

    AC-161 Customize left side menu on layout

--- src/Araneum/Bundle/MainBundle/Controller/DefaultController.php
+++ src/Araneum/Bundle/MainBundle/Controller/DefaultController.php
@@ -2,9 +2,14 @@
 
 namespace Araneum\Bundle\MainBundle\Controller;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Araneum\Bundle\AgentBundle\DependencyInjection\AraneumAgentExtension;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Araneum\Bundle\AgentBundle\DependencyInjection\Configuration;
 
 class DefaultController extends Controller
 {
@@ -18,5 +23,27 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return new Response();
+    }
+
+    /**
+     * @Route("/getMenu")
+     * @return Response
+     */
+    public function getMenuFromConfigAction()
+    {
+
+        $array = [
+            'Main' => [
+                'one' => 'oneIOtem',
+                'two' => 'secondItem'
+            ]
+        ];
+
+        $array = json_encode($array);
+
+        $response = new Response();
+        $response->setContent($array);
+
+        return $response;
     }
 }
