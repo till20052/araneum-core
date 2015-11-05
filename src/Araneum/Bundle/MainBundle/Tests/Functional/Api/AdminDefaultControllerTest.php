a1fe49e1e7aefc1aaf59c7ace4d167d9724e74da
Maksymc <maksymc@migfin.com>
2015-11-05 19:27:09 +0200

    AC-162 Create language action

---
+++ src/Araneum/Bundle/MainBundle/Tests/Functional/Api/AdminDefaultControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Araneum\Bundle\MainBundle\Tests\Functional\Api;
+
+use Araneum\Base\Tests\Controller\BaseController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class TranslatesApiGetTest
+ * @package Araneum\Bundle\MainBundle\Tests\Functional\Api
+ */
+class AdminDefaultControllerTest extends BaseController
+{
+    /**
+     * @var string uri to call rest api method
+     */
+    protected $configGetUri = '/en/admin/translates.json';
+
+    /**
+     * Test AdminDefaultController
+     *
+     * @runInSeparateProcess
+     */
+    public function testGetTranslateList()
+    {
+        $client = self::createAdminAuthorizedClient('api');
+        $client->request(
+            'GET',
+            $this->configGetUri
+        );
+
+        $response = $client->getResponse();
+        $content = $response->getContent();
+        $decoded = json_decode($content, true);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        $this->assertTrue(isset($decoded['admin']['sidebar.WELCOME']));
+        $this->assertTrue(is_string($decoded['admin']['sidebar.WELCOME']));
+    }
+}
