2b3ba74e638990f40a3991af73c56e90efa1834f
Artem Morozov <artemm@migfin.com>
2015-11-12 10:33:32 +0200

    AC-160 Develop Dashboard Widgets

--- src/Araneum/Bundle/MainBundle/Entity/Application.php
+++ src/Araneum/Bundle/MainBundle/Entity/Application.php
@@ -8,7 +8,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Araneum\Bundle\MainBundle\Entity\Cluster;
 
 /**
  * Application class
