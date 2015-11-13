01f51f90d935001cb99bfa3a0af390a07e310b60
Artem Morozov <artemm@migfin.com>
2015-11-13 10:13:58 +0200

    AC-157 Customize login ang recovery password pages

--- src/Araneum/Bundle/UserBundle/DependencyInjection/AraneumUserExtension.php
+++ src/Araneum/Bundle/UserBundle/DependencyInjection/AraneumUserExtension.php
@@ -22,11 +22,9 @@ class AraneumUserExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $xmlLoader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $xmlLoader->load('mailer.xml');
-
-        $ymlLoader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $ymlLoader->load('services.yml');
-        $ymlLoader->load('admin.yml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('mailer.yml');
+        $loader->load('services.yml');
+        $loader->load('admin.yml');
     }
 }
