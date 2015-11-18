919db900a7c0a64d28cc212a98ecf4ef89b22915
dmytrome <dmytrome@migfin.com>
2015-11-18 11:24:27 +0200

    fix test

--- src/Araneum/Bundle/AgentBundle/Tests/Functional/Api/CustomerApiControllerTest.php
+++ src/Araneum/Bundle/AgentBundle/Tests/Functional/Api/CustomerApiControllerTest.php
@@ -75,7 +75,6 @@ class CustomerApiControllerTest extends BaseController
                     'country' => 'country',
                     'email' => 'email@email.com',
                     'currency' => 'usd',
-                    'callback' => true,
                     'phone' => '380993222234'
                 ],
                 Response::HTTP_CREATED
@@ -87,7 +86,6 @@ class CustomerApiControllerTest extends BaseController
                     'country' => CustomerFixtures::TEST_COUNTRY,
                     'email' => CustomerFixtures::TEST_EMAIL,
                     'currency' => CustomerFixtures::TEST_CURRENCY,
-                    'callback' => true,
                     'phone' => CustomerFixtures::TEST_PHONE
                 ],
                 Response::HTTP_BAD_REQUEST
