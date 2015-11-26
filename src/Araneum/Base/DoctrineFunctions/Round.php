7f3e3940bad77eb78128c883fd9705cae38b48af
andreyp <andreyp@migfin.com>
2015-11-26 12:18:36 +0200

    AC-164 Connect application status charts

--- src/Araneum/Base/DoctrineFunctions/Round.php
+++ src/Araneum/Base/DoctrineFunctions/Round.php
@@ -11,37 +11,55 @@ use Doctrine\ORM\Query\Parser;
  * Class Round
  * @package Araneum\Base\DoctrineFunctions
  *
- * Round ::= "ROUND" "(" "ArithmeticPrimary" ")"
+ * Round ::= "ROUND" "(" "ArithmeticPrimary" "," "ArithmeticExpression" ")"
  */
 class Round extends FunctionNode
 {
-    private $arithmeticExpression;
-
     /**
-     * Parse
+     * simpleArithmeticExpression
      *
-     * @param Parser $parser
+     * @var mixed
+     * @access public
      */
-    public function parse(\Doctrine\ORM\Query\Parser $parser)
-    {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-
-        $this->arithmeticExpression = $parser->SimpleArithmeticExpression();
-
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
-    }
-
+    public $simpleArithmeticExpression;
     /**
-     * get SQL
+     * roundPrecission
+     *
+     * @var mixed
+     * @access public
+     */
+    public $roundPrecission;
+    /**
+     * getSql
      *
      * @param SqlWalker $sqlWalker
+     * @access public
      * @return string
      */
     public function getSql(SqlWalker $sqlWalker)
     {
-        return 'ROUND(' . $sqlWalker->walkSimpleArithmeticExpression(
-            $this->arithmeticExpression
-        ) . ')';
+        return 'ROUND(' .
+        $sqlWalker->walkSimpleArithmeticExpression($this->simpleArithmeticExpression) .','.
+        $sqlWalker->walkStringPrimary($this->roundPrecission) .
+        ')';
+    }
+    /**
+     * parse
+     *
+     * @param Parser $parser
+     * @access public
+     * @return void
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->simpleArithmeticExpression = $parser->SimpleArithmeticExpression();
+        $parser->match(Lexer::T_COMMA);
+        $this->roundPrecission = $parser->ArithmeticExpression();
+        if ($this->roundPrecission == null) {
+            $this->roundPrecission = 0;
+        }
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 }
