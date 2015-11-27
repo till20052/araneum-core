fed57ead36ce0af21c3ac873348f3c5170f8287e
Maksym Churkin <maksymc@migfin.com>
2015-11-27 15:38:09 +0200

    AC-226 Create server form builder

---
+++ src/Araneum/Base/Service/FromExporter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Araneum\Base\Service;
+
+
+class FromExporter
+{
+    private $serializer;
+
+    private $factory;
+
+    /**
+     * FromExporter constructor.
+     *
+     * @param $serializer
+     * @param $formFactory
+     */
+    public function __construct($serializer, $formFactory)
+    {
+        $this->serializer = $serializer;
+        $this->factory = $formFactory;
+    }
+
+    /**
+     * Export form to array
+     *
+     * @param $form
+     * @param array $data
+     * @return mixed
+     */
+    public function get($form, $data = [])
+    {
+        return $this->serializer->toArray(
+            $this->factory->create($form, $data)->createView()
+        );
+    }
+}
