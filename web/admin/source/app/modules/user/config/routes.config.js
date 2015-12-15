a065d44371a87ca0b7c81dd5fe5f31c98e7e1f2c
andreyp <andreyp@migfin.com>
2015-12-15 11:49:11 +0200

    AC-256 Runner entity

--- web/admin/source/app/modules/user/config/routes.config.js
+++ web/admin/source/app/modules/user/config/routes.config.js
@@ -5,42 +5,51 @@
 
 
 (function () {
-	'use strict';
+    'use strict';
 
-	angular
-		.module('app')
-		.config(routesConfig);
+    angular
+        .module('app')
+        .config(routesConfig);
 
-	routesConfig.$inject = ['$stateProvider', 'RouteHelpersProvider'];
-	function routesConfig($stateProvider, helper) {
-		$stateProvider
-			.state('app.users', {
-				url: '/users',
-				title: 'Users',
-				initialize: '/user/manage/users/init.json',
-				templateUrl: helper.basepath('grid-template.html'),
-				resolve: helper.resolveFor('datatables', 'whirl')
-			})
-			.state('login', {
-				url: '/login',
-				title: 'Authorization',
-				templateUrl: helper.basepath('users/login.html'),
-				resolve: helper.resolveFor('whirl'),
-				defaultState: 'app.dashboard'
-			})
-			.state('resetting', {
-				url: '/resetting',
-				title: 'Recover',
-				templateUrl: helper.basepath('users/resettingBase.html'),
-				resolve: helper.resolveFor('whirl')
-			})
-			.state('reset', {
-				url: '/resetting/reset/{token}',
-				title: 'Recover',
-				templateUrl: helper.basepath('users/resettingBase.html'),
-				resolve: helper.resolveFor('whirl')
-			});
-	}
+    routesConfig.$inject = ['$stateProvider', 'RouteHelpersProvider'];
+    function routesConfig($stateProvider, helper) {
+        $stateProvider
+            .state('app.users', {
+                url: '/users',
+                title: 'Users',
+                initialize: '/user/manage/users/init.json',
+                templateUrl: helper.basepath('grid-template.html'),
+                resolve: helper.resolveFor('datatables', 'whirl'),
+                controller: 'UserTableController'
+            })
+            .state('app.locales', {
+                url: '/locales',
+                title: 'Locales',
+                initialize: '/user/manage/locales/init.json',
+                templateUrl: helper.basepath('grid-template.html'),
+                resolve: helper.resolveFor('datatables', 'whirl'),
+                controller: 'LocalesController'
+            })
+            .state('login', {
+                url: '/login',
+                title: 'Authorization',
+                templateUrl: helper.basepath('users/login.html'),
+                resolve: helper.resolveFor('whirl'),
+                defaultState: 'app.dashboard'
+            })
+            .state('resetting', {
+                url: '/resetting',
+                title: 'Recover',
+                templateUrl: helper.basepath('users/resettingBase.html'),
+                resolve: helper.resolveFor('whirl')
+            })
+            .state('reset', {
+                url: '/resetting/reset/{token}',
+                title: 'Recover',
+                templateUrl: helper.basepath('users/resettingBase.html'),
+                resolve: helper.resolveFor('whirl')
+            });
+    }
 
 })();
 
