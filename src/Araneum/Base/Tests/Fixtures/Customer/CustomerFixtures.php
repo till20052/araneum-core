5fcae0a96f7a56d5d56d3c9d2a6fe28b630294be
Maksym Churkin <maksymc@migfin.com>
2015-12-17 13:49:42 +0200

    AC-265 Update Customer API POST method (change deployment:run)

--- src/Araneum/Base/Tests/Fixtures/Customer/CustomerFixtures.php
+++ src/Araneum/Base/Tests/Fixtures/Customer/CustomerFixtures.php
@@ -19,14 +19,14 @@ class CustomerFixtures extends AbstractFixture implements FixtureInterface, Depe
     const TEST_LAST_NAME    = 'TestCustomerLastName';
     const TEST_EMAIL        = 'TestCustomer@Email.com';
     const TEST_PHONE        = '380998887766';
-    const TEST_COUNTRY      = 'TestCustomerCountry';
+    const TEST_COUNTRY      = '1';
     const TEST_CALLBACK     = true;
     const TEST_CURRENCY     = 'usd';
     const TEST_2_FIRST_NAME = 'AnotherCustomerFirstName';
     const TEST_2_LAST_NAME  = 'AnotherCustomerLastName';
     const TEST_2_EMAIL      = 'TestCustomer2@Email.com';
     const TEST_2_PHONE      = '380998887766';
-    const TEST_2_COUNTRY    = 'AnotherCustomerCountry';
+    const TEST_2_COUNTRY    = '22';
     const TEST_2_CURRENCY   = 'usd';
     const TEST_2_CALLBACK   = false;
 
