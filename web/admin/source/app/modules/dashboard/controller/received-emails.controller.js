c601504b567650b6f6410e3c13cc0d2f9acfe931
artemm <artemm@migfin.com>
2015-12-24 19:51:41 +0200

    AC-234 Lead chart widget

--- web/admin/source/app/modules/dashboard/controller/received-emails.controller.js
+++ web/admin/source/app/modules/dashboard/controller/received-emails.controller.js
@@ -2,82 +2,90 @@
  * Created by artemm on 30.11.15.
  */
 (function (ng) {
-	'use strict';
+    'use strict';
 
-	ng.module('app.dashboard')
-		.controller('ReceivedEmailsController', ReceivedEmailsController);
+    ng.module('app.dashboard')
+        .controller('ReceivedEmailsController', ReceivedEmailsController);
 
-	ReceivedEmailsController.$inject = ['$scope', 'DashboardService'];
+    ReceivedEmailsController.$inject = ['$scope', 'DashboardService', '$translate'];
 
-	/**
-	 * Received Emails Controller
-	 *
-	 * @param $scope
-	 * @param DashboardService
-	 * @constructor
-	 */
-	function ReceivedEmailsController($scope, DashboardService) {
+    /**
+     * Received Emails Controller
+     *
+     * @param $scope
+     * @param DashboardService
+     * @param $translate
+     * @constructor
+     */
+    function ReceivedEmailsController($scope, DashboardService, $translate) {
 
-		/**
-		 * Constructor
-		 */
-		(function (vm) {
+        /**
+         * Constructor
+         */
+        (function (vm) {
 
-			vm.error = '';
-			vm.data = [];
-			vm.count = 0;
-			vm.options = {
-				series: {
-					lines: {
-						show: true,
-						fill: 0.8
-					},
-					points: {
-						show: true,
-						radius: 4
-					}
-				},
-				grid: {
-					borderColor: '#eee',
-					borderWidth: 1,
-					hoverable: true,
-					backgroundColor: '#fcfcfc'
-				},
-				tooltip: true,
-				tooltipOpts: {
-					content: function (label, x, y) {
-						return x + ' : ' + y;
-					}
-				},
-				xaxis: {
-					tickColor: '#fcfcfc',
-					mode: 'categories'
-				},
-				yaxis: {
-					min: 0,
-					tickColor: '#eee',
-					position: ($scope.app.layout.isRTL ? 'right' : 'left'),
-					tickFormatter: function (v) {
-						return v + ' visitors';
-					}
-				},
-				shadowSize: 0
-			};
+            vm.error = '';
+            vm.data = [];
+            vm.count = 0;
+            vm.options = {
+                series: {
+                    lines: {
+                        show: true,
+                        fill: 0.8
+                    },
+                    points: {
+                        show: true,
+                        radius: 4
+                    }
+                },
+                grid: {
+                    borderColor: '#eee',
+                    borderWidth: 1,
+                    hoverable: true,
+                    backgroundColor: '#fcfcfc'
+                },
+                tooltip: true,
+                tooltipOpts: {
+                    content: function (label, x, y) {
+                        return x + ' : ' + y;
+                    }
+                },
+                xaxis: {
+                    tickColor: '#fcfcfc',
+                    mode: 'categories'
+                },
+                yaxis: {
+                    min: 0,
+                    tickColor: '#eee',
+                    position: ($scope.app.layout.isRTL ? 'right' : 'left'),
+                    tickFormatter: function (v) {
+                        return v + ' ' + vm.captions.emails;
+                    }
+                },
+                shadowSize: 0
+            };
 
-			$scope.onLoading = true;
+            $scope.onLoading = true;
+            vm.captions = {
+                emails: 'emails'
+            };
 
-			DashboardService.onDataLoaded(function(response){
-				$scope.onLoading = false;
-				vm.count = response.data.statistics.receivedEmails.count;
-				vm.data = response.data.statistics.receivedEmails.data;
-				DashboardService.assignColorsByLabel(vm.data);
-			}, function(error){
-				$scope.onLoading = false;
-				vm.error = error
-			});
+            DashboardService.onDataLoaded(function (response) {
+                $scope.onLoading = false;
+                vm.count = response.data.statistics.receivedEmails.count;
+                vm.data = response.data.statistics.receivedEmails.data;
+                DashboardService.assignColorsByLabel(vm.data);
+            }, function (error) {
+                $scope.onLoading = false;
+                vm.error = error;
+            });
 
-		})($scope);
+            $translate('admin.dashboard.widget.EMAILS').then(function (value) {
+                vm.captions.emails = value;
+            });
 
-	}
+        })($scope);
+
+    }
 
 })(angular);
