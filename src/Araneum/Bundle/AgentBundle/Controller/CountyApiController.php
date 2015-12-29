914d12eaa4091d089816feaf6758dcaa890b88ef
artemm <artemm@migfin.com>
2015-12-29 12:03:20 +0200

    AC-255 GET Api method for getting countries

--- src/Araneum/Bundle/AgentBundle/Controller/CountyApiController.php
+++ src/Araneum/Bundle/AgentBundle/Controller/CountyApiController.php
@@ -6,7 +6,6 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class CountyApiController
