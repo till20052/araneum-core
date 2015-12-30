b83a4f80bda13a1751c322ea179f2b27bf6a93a9
artemm <artemm@migfin.com>
2015-12-30 15:25:16 +0200

    AC-219 Need to remove the possibility use a second time the same reset link

--- web/admin/source/app/modules/user/controller/resetting.controller.js
+++ web/admin/source/app/modules/user/controller/resetting.controller.js
@@ -1,137 +1,138 @@
 (function (angular) {
-	'use strict';
+    'use strict';
 
-	angular
-		.module('app.users')
-		.controller('ResettingController', ResettingController);
+    angular
+        .module('app.users')
+        .controller('ResettingController', ResettingController);
 
-	ResettingController.$inject = ['$http', '$state', '$stateParams'];
-	function ResettingController($http, $state, $stateParams) {
+    ResettingController.$inject = ['$http', '$state', '$stateParams'];
+    function ResettingController($http, $state, $stateParams) {
 
-		// Initialize viewModel
-		(function (vm) {
+        // Initialize viewModel
+        (function (vm) {
 
-			vm.inLoading = false;
-			vm.error = '';
-			vm.username = '';
-			vm.maskedEmail = '';
+            vm.inLoading = false;
+            vm.error = '';
+            vm.username = '';
+            vm.maskedEmail = '';
 
-			vm.view = {
-				request: true,
-				checkMail: false,
-				reset: false
-			};
+            vm.view = {
+                request: true,
+                checkMail: false,
+                reset: false
+            };
 
-			if (typeof $stateParams.token != 'undefined') {
-				vm.view.request = false;
-				vm.view.reset = true;
-			}
+            if (typeof $stateParams.token != 'undefined') {
+                vm.view.request = false;
+                vm.view.reset = true;
+            }
 
-		})(this);
+        })(this);
 
-		// Initialize subView: Send Email
-		(function (vm) {
+        // Initialize subView: Send Email
+        (function (vm) {
 
-			vm.sendEmailAction = sendEmailAction;
+            vm.sendEmailAction = sendEmailAction;
 
-			function sendEmailAction() {
-				var form = vm['request'];
+            function sendEmailAction() {
+                var form = vm['request'];
 
-				vm.error = '';
+                vm.error = '';
 
-				if (form.$valid) {
+                if (form.$valid) {
 
-					vm.inLoading = true;
+                    vm.inLoading = true;
 
-					$http.post('/resetting/send-email', {
-						username: vm.username
-					}).then(function (response) {
+                    $http.post('/resetting/send-email', {
+                        username: vm.username
+                    }).then(function (response) {
 
-						vm.inLoading = false;
-						vm.view.request = false;
-						vm.view.checkMail = true;
-						vm.maskedEmail = response.data.email;
+                        vm.inLoading = false;
+                        vm.view.request = false;
+                        vm.view.checkMail = true;
+                        vm.maskedEmail = response.data.email;
 
-					}, function (response) {
+                    }, function (response) {
 
-						vm.inLoading = false;
-						vm.error = response.data.error;
+                        vm.inLoading = false;
+                        vm.error = response.data.error;
 
-					});
-				}
-				else {
-					form.username.$dirty = true;
-				}
-			}
+                    });
+                }
+                else {
+                    form.username.$dirty = true;
+                }
+            }
 
-		})(this);
+        })(this);
 
-		// Initialize subView: Reset
-		(function (vm) {
+        // Initialize subView: Reset
+        (function (vm) {
 
-			var url = '/resetting/reset/' + $stateParams.token;
-			var csrfToken = {};
-			var fosUi = {
-				first: {label: 'form.new_password', name: '', full_name: '', ng: '', value: ''},
-				second: {label: 'form.new_password_confirmation', name: '', full_name: '', ng: '', value: ''}
-			};
+            var url = '/resetting/reset/' + $stateParams.token;
+            var csrfToken = {};
+            var fosUi = {
+                first: {label: 'form.new_password', name: '', full_name: '', ng: '', value: ''},
+                second: {label: 'form.new_password_confirmation', name: '', full_name: '', ng: '', value: ''}
+            };
 
-			vm.fosUi = fosUi;
+            vm.fosUi = fosUi;
+            vm.showForm = true;
 
-			vm.onViewLoaded = onViewLoaded;
-			vm.resetAction = resetAction;
+            vm.onViewLoaded = onViewLoaded;
+            vm.resetAction = resetAction;
 
-			function onViewLoaded() {
-				vm.inLoading = true;
-				$http
-					.post(url)
-					.success(function (response) {
-						angular.forEach(response, function(ui){
-							if (ui.name == '_token') {
-								csrfToken = ui;
-								return;
-							}
-							this[ui.name] = angular.extend(this[ui.name], ui);
-						}, fosUi);
-						vm.inLoading = false;
-					})
-					.error(function (response) {
-						vm.inLoading = false;
-						vm.error = response.error
-					});
-			}
+            function onViewLoaded() {
+                vm.inLoading = true;
+                $http
+                    .post(url)
+                    .success(function (response) {
+                        angular.forEach(response, function (ui) {
+                            if (ui.name == '_token') {
+                                csrfToken = ui;
+                                return;
+                            }
+                            this[ui.name] = angular.extend(this[ui.name], ui);
+                        }, fosUi);
+                        vm.inLoading = false;
+                    })
+                    .error(function (response) {
+                        vm.inLoading = false;
+                        vm.showForm = false;
+                    });
+            }
 
-			function resetAction() {
-				var data = {};
+            function resetAction() {
+                var data = {};
 
-				vm.inLoading = true;
+                vm.inLoading = true;
 
-				angular.forEach(fosUi, function (ui) {
-					this[ui.full_name] = ui.ng;
-				}, data);
+                angular.forEach(fosUi, function (ui) {
+                    this[ui.full_name] = ui.ng;
+                }, data);
 
-				data[csrfToken.full_name] = csrfToken.value;
+                data[csrfToken.full_name] = csrfToken.value;
 
-				$http
-					.post(
-						url,
-						$.param(data),
-						{headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-					)
-					.success(function (response, statusCode) {
-						if (statusCode != 202) {
-							vm.inLoading = false;
-							return;
-						}
-						$state.go('login');
-					})
-					.error(function (response) {
-						vm.inLoading = false;
-						vm.error = response.error;
-					});
-			}
+                $http
+                    .post(
+                        url,
+                        $.param(data),
+                        {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+                    )
+                    .success(function (response, statusCode) {
+                        if (statusCode != 202) {
+                            vm.inLoading = false;
+                            return;
+                        }
+                        $state.go('login');
+                    })
+                    .error(function (response) {
+                        vm.inLoading = false;
+                        vm.error = response.error;
+                    });
+            }
 
-		})(this);
+        })(this);
 
-	}
+    }
 })(angular);
