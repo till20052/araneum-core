cd782c2f78d425789c495ab6723e0d9ed9424b60
artemm <artemm@migfin.com>
2016-01-05 18:15:38 +0200

    AC-267 Fix bug associated with api routes (in Api Doc)

--- src/Araneum/Bundle/MainBundle/Controller/AdminDefaultController.php
+++ src/Araneum/Bundle/MainBundle/Controller/AdminDefaultController.php
@@ -52,13 +52,21 @@ class AdminDefaultController extends Controller
      *          "name" = "_format",
      *          "dataType" = "json",
      *          "description" = "Output format must be json"
-     *      }
+     *      },
+     *     {
+     *          "name" = "_locale",
+     *          "dataType" = "string",
+     *          "description" = "Content language. Possible values: en|ru"
+     *     }
      *   },
-     *   tags={"AdminApi"}
+     *   tags={"AdminApi"},
+     * )
+     *
+     * @Rest\Get(
+     *     "{_locale}/manage/translates.json",
+     *     name="araneum_admin_translation_default_locale"
      * )
      * @Security("has_role('ROLE_ADMIN')")
-     * @Rest\Get("/manage/translates.json",         name="araneum_admin_translations")
-     * @Rest\Get("%locale%/manage/translates.json", name="araneum_admin_translation_default_locale")
      *
      * @return JsonResponse
      */
