89a9494d112acea9bd73fd007d6d72a0a150f7a1
artemm <artemm@migfin.com>
2016-01-05 18:28:01 +0200

    AC-244 Need to create redirect to the login when session end

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,7 +26,8 @@ class AppKernel extends Kernel
             new Lexik\Bundle\MaintenanceBundle\LexikMaintenanceBundle(),
             new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
-	        new Misd\GuzzleBundle\MisdGuzzleBundle(),
+            new Misd\GuzzleBundle\MisdGuzzleBundle(),
+            new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
             new Ali\DatatableBundle\AliDatatableBundle(),
             new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
 
