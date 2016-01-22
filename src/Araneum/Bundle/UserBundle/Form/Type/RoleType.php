45cb7635d6c2afb5cea81b6cd6a257f0c0f60978
alamast <vladislavg@migfin.com>
2016-01-22 10:05:59 +0200

    AC-264

--- src/Araneum/Bundle/UserBundle/Form/Type/RoleType.php
+++ src/Araneum/Bundle/UserBundle/Form/Type/RoleType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * Class UserType
+ * Class RoleType
  *
  * @package Araneum\Bundle\UserBundle\Form\Type
  */
