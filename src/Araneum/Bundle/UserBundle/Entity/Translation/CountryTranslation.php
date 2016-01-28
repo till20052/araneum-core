456af16d82b67b7720ee63b1407d4cb3f4fddbf0
alamast <vladislavg@migfin.com>
2016-01-28 19:25:52 +0200

    AC-297 Command for get all new Spot customer

---
+++ src/Araneum/Bundle/UserBundle/Entity/Translation/CountryTranslation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Araneum\Bundle\UserBundle\Entity\Translation;
+
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="araneum_user_country_translations", indexes={
+ *      @ORM\Index(name="araneum_user_country_translation_idx", columns={"locale", "object_id", "field"})
+ * })
+ * @ORM\Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
+ */
+class CountryTranslation extends AbstractPersonalTranslation
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Araneum\Bundle\UserBundle\Entity\Country", inversedBy="translations")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $object;
+
+    /**
+     * Convenient constructor
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale, $field, $value)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+}
---
+++ src/Araneum/Bundle/UserBundle/Repository/CountryRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Araneum\Bundle\UserBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr;
+
+/**
+ * Class CountryRepository
+ * @package Araneum\Bundle\MainBundle\Repository
+ */
+class CountryRepository extends EntityRepository
+{
+}
