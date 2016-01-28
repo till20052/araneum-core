0d0053f035a4458efacee7a43fcaccb67a385fb6
alamast <vladislavg@migfin.com>
2016-01-28 12:25:18 +0200

    AC-297 Command for get all new Spot customer

--- src/Araneum/Bundle/MainBundle/Command/Spot/GetNewCustomersFromSpotCommand.php
+++ src/Araneum/Bundle/MainBundle/Command/Spot/GetNewCustomersFromSpotCommand.php
@@ -7,7 +7,13 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use MikSoftware\DaemonBundle\Commnad\DaemonizedCommand;
-
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Filesystem\Filesystem;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\QueryBuilder;
 /**
  * Class GetNewCustomersFromSpot
  *
@@ -15,6 +21,9 @@ use MikSoftware\DaemonBundle\Commnad\DaemonizedCommand;
  */
 class GetNewCustomersFromSpotCommand extends DaemonizedCommand
 {
+
+    const DEFAULT_PERIOD = 'P1Y';
+    const APPLICATION_ID = 1;
     /**
      * Configures command.
      */
@@ -23,6 +32,20 @@ class GetNewCustomersFromSpotCommand extends DaemonizedCommand
         $this
             ->setName('araneum:spot:customers')
             ->setDescription('get Customers.')
+            ->addOption(
+                'application',
+                '-a',
+                InputOption::VALUE_REQUIRED,
+                'Application to execute',
+                self::APPLICATION_ID
+            )
+            ->addOption(
+                'period',
+                '-p',
+                InputOption::VALUE_REQUIRED,
+                'Period of customers registration',
+                self::DEFAULT_PERIOD
+            )
             ->setHelp('Usage <info>php app/console <name> <period>application_id</period> <period>time</period> start|stop|restart</info>');
     }
 
@@ -33,8 +56,12 @@ class GetNewCustomersFromSpotCommand extends DaemonizedCommand
      */
     protected function daemonLogic()
     {
-        $app = $this->getContainer()->get('doctrine')->getRepository('AraneumMainBundle:Application')->findOneById(1);
+        $application = $this->input->getOption('application');
+        $period = $this->input->getOption('period');
+        $app = $this->getContainer()->get('doctrine')->getRepository('AraneumMainBundle:Application')->findOneById($application);
         $commandRunner = $this->getContainer()->get('araneum.spot.api.customer.service');
-        $commandRunner->getAllCustomersByPeriod($app,'P1Y');
+        $commandRunner->getAllCustomersByPeriod($app, $period);
+        $this->getContainer()->get('logger')->info('Daemon is running!');
+        $this->daemon->iterate(10);
     }
 }
