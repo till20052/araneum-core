f8530c2b2a612fc939ccdc44964e6a0580fa2a58
alamast <vladislavg@migfin.com>
2016-01-29 13:49:24 +0200

    AC-297 Command for get all new Spot customer

--- src/Araneum/Bundle/AgentBundle/Service/Applications/ApplicationsApiService.php
+++ src/Araneum/Bundle/AgentBundle/Service/Applications/ApplicationsApiService.php
@@ -2,13 +2,10 @@
 
 namespace Araneum\Bundle\AgentBundle\Service\Applications;
 
-use Araneum\Base\Service\RabbitMQ\SpotCustomerLoginProducerService;
-use Araneum\Base\Service\RabbitMQ\SpotCustomerProducerService;
-use Araneum\Base\Service\RabbitMQ\SpotProducerService;
-use Araneum\Bundle\AgentBundle\Entity\Customer;
-use Araneum\Bundle\AgentBundle\Entity\CustomerLog;
-use Araneum\Bundle\AgentBundle\Entity\Lead;
-use Araneum\Bundle\MainBundle\Entity\Application;
+
+use Araneum\Base\Service\RabbitMQ\ApiCustomerProducerService;
+use Araneum\Base\Service\RabbitMQ\ApiCustomerConsumerService;
+
 use Araneum\Base\Service\Spot\SpotApiSenderService;
 use Doctrine\ORM\EntityManager;
 use fixtures\App;
@@ -16,11 +13,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
- * Class SpotOptionService
+ * Class ApplicationsApiService
  *
  * @package Araneum\Bundle\AgentBundle\Service
  */
-class SpotOptionService
+class ApplicationsApiService
 {
 
     protected $customerLoginProducerService;
@@ -30,7 +27,7 @@ class SpotOptionService
     protected $spotCustomerProducerService;
 
     /**
-     * @var SpotOptionService
+     * @var ApplicationsApiService
      */
     protected $spotApiSenderService;
 
@@ -64,126 +61,5 @@ class SpotOptionService
         $this->spotProducerService = $spotProducerService;
         $this->spotApiSenderService = $spotApiSenderService;
         $this->entityManager = $entityManager;
-    }
-
-    /**
-     * SpotOption Login
-     *
-     * @param Customer $customer
-     * @return array|bool
-     */
-    public function login(Customer $customer)
-    {
-        return $this->customerLoginProducerService->publish($customer);
-    }
-
-    /**
-     * Reset Customer Password on SpotOption
-     *
-     * @param Customer $customer
-     * @return bool
-     */
-    public function customerResetPassword(Customer $customer)
-    {
-        $customerData = [
-            'MODULE' => 'Customer',
-            'COMMAND' => 'edit',
-            'customerId' => $customer->getSpotId(),
-            'password' => $customer->getPassword(),
-        ];
-
-        return $this->spotCustomerProducerService->publish($customerData, $customer, CustomerLog::ACTION_RESET_PASSWORD);
-    }
-
-    /**
-     * Send customer creation data to SpotOption with RabbitMQ
-     *
-     * @param Customer $customer
-     * @return string|true
-     */
-    public function customerCreate(Customer $customer)
-    {
-        $customerData = [
-            'MODULE' => 'Customer',
-            'COMMAND' => 'add',
-            'FirstName' => $customer->getFirstName(),
-            'LastName' => $customer->getLastName(),
-            'email' => $customer->getEmail(),
-            'password' => $customer->getPassword(),
-            'Phone' => $customer->getPhone(),
-            'Country' => $customer->getCountry(),
-            'currency' => $customer->getCurrency(),
-        ];
-
-        if ($customer->getBirthday()) {
-            $customerData['birthday'] = $customer->getBirthday()->format('Y-m-d');
-        }
-
-        return $this->spotCustomerProducerService->publish($customerData, $customer, CustomerLog::ACTION_CREATE);
-    }
-
-    /**
-     * Get countries method
-     *
-     * @param string $appKey
-     * @return mixed
-     * @throws NotFoundHttpException in case if can't found by application appKey
-     */
-    public function getCountries($appKey)
-    {
-        /** @var Application $application */
-        $application = $this->entityManager
-            ->getRepository('AraneumMainBundle:Application')
-            ->findOneByAppKey($appKey);
-
-        if (empty($application)) {
-            throw new NotFoundHttpException('Not Application found for this appKey', null, Response::HTTP_NOT_FOUND);
-        }
-
-        return $this->spotApiSenderService->get(
-            [
-                'MODULE' => 'Country',
-                'COMMAND' => 'view',
-            ],
-            $application->getSpotCredential()
-        );
-    }
-
-    /**
-     * Send Lead creation data to SpotOption with RabbitMQ
-     *
-     * @param Lead $lead
-     * @return bool|string
-     */
-    public function leadCreate(Lead $lead)
-    {
-        $leadData = [
-            'MODULE' => 'Lead',
-            'COMMAND' => 'add',
-            'FirstName' => $lead->getFirstName(),
-            'LastName' => $lead->getLastName(),
-            'Country' => $lead->getCountry(),
-            'Phone' => $lead->getPhone(),
-            'email' => $lead->getEmail(),
-        ];
-
-        return $this->spotProducerService->publish($leadData, $lead->getApplication()->getSpotCredential());
-    }
-
-    /**
-     * Get customers from spot by filter
-     *
-     * @param Application   $application
-     * @param array         $filterOptions
-     * @return \Guzzle\Http\Message\Response
-     */
-    public function getCustomersByFilter($application, $filterOptions)
-    {
-        $data = [
-            'MODULE' => 'Customer',
-            'COMMAND' => 'view',
-            'FILTER' => $filterOptions
-        ];
-        return $this->spotApiSenderService->send($data, $application->getSpotCredential());
     }
 }
