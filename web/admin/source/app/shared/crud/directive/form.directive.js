0a8825e5a2cd6a1a41a760f6ad2a8622409e5c04
artemm <artemm@migfin.com>
2016-02-02 16:12:59 +0200

    AC-302 Fix problems builders

--- web/admin/source/app/shared/crud/directive/form.directive.js
+++ web/admin/source/app/shared/crud/directive/form.directive.js
@@ -9,12 +9,6 @@
 
     /**
      * CRUD From Directive
-     *
-     * @param CRUDFormLoader
-     * @param helper
-     * @param $compile
-     * @returns {object}
-     * @constructor
      */
     function CRUDFormDirective(CRUDFormLoader, $compile) {
         var children = {
@@ -59,42 +53,24 @@
                     }
                 });
             }
-
-            /*
-            var structure = scope.structure;
-
-            if (structure instanceof Object) {
-                if (structure.hasOwnProperty('source')) {
-                    CRUDFormLoader
-                        .setUrl(structure.source)
-                        .load({
-                            onSuccess: function (structure) {
-                                createForm(controller, iElement, structure)(scope);
-                                CRUDFormLoader.clearPromise();
-                            }
-                        });
-                }
-            }
-
-            scope.$watch(function () {
-                return scope.structure instanceof Object;
-            }, function (isReady) {
-                if (isReady) {
-                    createForm(controller, iElement, scope.structure)(scope);
-                }
-            });
-            */
         }
 
         /**
          * Create form
          */
         function createForm(data) {
-            var form = $('<form />');
+            var form = $('<form />').attr({
+                name: data.vars.name,
+                novalidate: ''
+            });
 
             for(var id in data.children){
                 var type = data.children[id].vars.block_prefixes[1],
                     child = createChild(type, data.children[id].vars);
+
+                $(child).attr({
+                    'ng-model': 'controller.form.data.' + id
+                });
 
                 form.append(child);
             }
@@ -109,58 +85,13 @@
             }));
 
             return form;
-            /*
-            if (
-                structure.hasOwnProperty('vars') &&
-                structure.vars instanceof Object
-            ) {
-                $('>form', placement).attr('name', 'controller.form.validation');
-                controller.form = angular.extend(controller.form, {
-                    name: structure.vars.name,
-                    method: structure.vars.method,
-                    action: structure.vars.action
-                });
-            }
-
-            if (structure.hasOwnProperty('children')) {
-                for (var key in structure.children) {
-                    if (structure.children.hasOwnProperty(key)) {
-                        controller.form.origin[key] = structure.children[key].vars;
-                        controller.form.data[key] = structure.vars.value[key];
-                        $('*[id="' + key + '"]', createUiControlSection(
-                            $('>form', placement),
-                            structure.children[key].vars.block_prefixes[1],
-                            structure.children[key].vars
-                        )).attr({
-                            name: structure.children[key].vars.full_name,
-                            'ng-model': 'controller.form.data.' + key
-                        });
-                    }
-                }
-            }
-
-            createUiControlSection($('>form', placement), 'submit', {
-                submit: {
-                    label: 'admin.general.SAVE',
-                    onClick: controller.form.submit
-                },
-                cancel: {
-                    label: 'admin.general.CANCEL',
-                    onClick: controller.form.cancel
-                }
-            });
-
-            return function (scope) {
-                $compile($('>form', placement))(scope);
-            };
-            */
         }
 
         /**
          * Create form child
          */
         function createChild(type, data) {
-            /* jshint -W106, -W014, eqeqeq: false, validthis: true */
+            /* jshint eqeqeq: false */
             if (!children.hasOwnProperty(type)) {
                 return console.error('[ERROR]: Try to create form child by type: ' + type + ', but this child doesn\'t defined');
             }
@@ -199,47 +130,6 @@
             }
 
             return group;
-
-            /*
-            var section = ()[element](angular.extend(
-                {
-                    uid: config.name,
-                    label: config.label
-                },
-                (function (ext) {
-                    if (config.attr instanceof Object) {
-                        ['translateLabel', 'placeholder'].forEach(function (key) {
-                            if (!config.attr.hasOwnProperty(key))
-                                return;
-                            ext[key] = config.attr[key];
-                        });
-                    }
-                    return ext;
-                })({}),
-                (function (ext) {
-                    if (element == 'submit') {
-                        ext = config;
-                    }
-                    return ext;
-                })({})
-            ));
-
-            if (element == 'hidden') {
-                return form.prepend(section);
-            }
-
-            section = $('<div class="form-group" />')
-                .append(section);
-
-            if (element == 'submit') {
-                section.addClass('mb0');
-            }
-
-            // @todo need a condition for render different form layout
-            //section = $('<div class="col-lg-6" />').append(section);
-
-            return form.append(section);
-            */
         }
 
         /**
