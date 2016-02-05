8ef70322bab05e1c5de7bb2385a1ca77e6e23111
alamast <vladislavg@migfin.com>
2016-02-05 14:37:50 +0200

    AC-297

--- src/Araneum/Bundle/MainBundle/DataFixtures/ORM/CountryData.php
+++ src/Araneum/Bundle/MainBundle/DataFixtures/ORM/CountryData.php
@@ -14,6 +14,13 @@ use RuntimeException;
  */
 class LoadCountryData extends AbstractFixture
 {
+    public function getDependencies()
+    {
+        return [
+            'Araneum\Bundle\MainBundle\DataFixtures\ORM\ApplicationData',
+        ];
+    }
+
     /**
      * Fixtures for country
      *
@@ -22,32 +29,35 @@ class LoadCountryData extends AbstractFixture
      */
     public function load(ObjectManager $manager)
     {
-        $application = $manager->getRepository('AraneumMainBundle:Application')->findOneById(1);
-        $data = $this->container->get('araneum.base.spot_api')->get(
-            [
-                'MODULE' => 'Country',
-                'COMMAND' => 'view',
-            ],
-            $application->getSpotCredential()
-        );
-        if (!empty($data)) {
-            foreach ($data as $countries) {
-                $country = $manager->getRepository('AraneumAgentBundle:Country')->findOneBy(
-                    ['name' => $countries['iso']]
-                );
-                if (empty($country)) {
-                    $country = new Country();
-                    $country->setName($countries['iso']);
-                    $country->setTitle($countries['name']);
-                    $country->setPhoneCode(!empty($countries['prefix']) ? $countries['prefix'] : null);
-                    $country->setSpotId($countries['id']);
-                    $manager->persist($country);
+        $application = $manager
+            ->getRepository('AraneumMainBundle:Application')
+            ->findOneByName('Ultratrade');
+        if (!empty($app)) {
+            $data = $this->container->get('araneum.base.spot_api')->get(
+                [
+                    'MODULE' => 'Country',
+                    'COMMAND' => 'view',
+                ],
+                $application->getSpotCredential()
+            );
+            if (!empty($data)) {
+                foreach ($data as $countries) {
+                    $country = $manager->getRepository('AraneumAgentBundle:Country')->findOneBy(
+                        ['name' => $countries['iso']]
+                    );
+                    if (empty($country)) {
+                        $country = new Country();
+                        $country->setName($countries['iso']);
+                        $country->setTitle($countries['name']);
+                        $country->setPhoneCode(!empty($countries['prefix']) ? $countries['prefix'] : null);
+                        $country->setSpotId($countries['id']);
+                        $manager->persist($country);
+                    }
                 }
+            } else {
+                throw new RuntimeException('Failed to parse countries');
             }
-        } else {
-            throw new RuntimeException('Failed to parse countries');
+            $manager->flush();
         }
-
-        $manager->flush();
     }
 }
