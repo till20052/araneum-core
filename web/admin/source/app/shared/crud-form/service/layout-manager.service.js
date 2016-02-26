e42c4f76eb68697b5ef6e107bffd5d67421b39b9
artemm <artemm@migfin.com>
2016-02-26 19:08:31 +0200

    AC-302 Fix problems builders

---
+++ web/admin/source/app/shared/crud-form/service/layout-manager.service.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('crud.form')
+        .service('layoutManager', layoutManager);
+
+    layoutManager.$inject = [];
+
+    /**
+     *
+     */
+    function layoutManager() {
+        /* jshint validthis: true */
+        return function (name) {
+
+        };
+
+        function Inline(){
+
+        }
+
+        function Horizontal() {
+
+        }
+    }
+
+})();
