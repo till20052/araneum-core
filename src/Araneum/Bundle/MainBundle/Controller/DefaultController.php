3450d358eb49b61e31cec3d3606e1a3802ac636a
yevheniik <yevheniik@migfin.com>
2016-03-02 11:16:05 +0200

    AC-150 Customize error pages. Rewrite code for handling custom exception messages

--- src/Araneum/Bundle/MainBundle/Controller/DefaultController.php
+++ src/Araneum/Bundle/MainBundle/Controller/DefaultController.php
@@ -21,4 +21,37 @@ class DefaultController extends Controller
     {
         return $this->render('admin.layout.html.twig');
     }
+
+    /**
+     * Render custom exception messages if they are exists
+     *
+     * @param string $code Exception code
+     * @param string $text Exception text
+     * @param string $message Exception message
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function exceptionCustomMessagesAction($code, $text, $message)
+    {
+        $translateAlias = $code.'.status_text';
+        $translator = $this->get('translator');
+        $statusText = $translator->trans($translateAlias, [], 'exceptions');
+        if ($translateAlias == $statusText) {
+            $statusText = $text;
+        }
+
+        $translateAlias = $code.'.exception_message';
+        $exceptionMessage = $translator->trans($translateAlias, [], 'exceptions');
+        if ($translateAlias == $exceptionMessage) {
+            $exceptionMessage = $message;
+        }
+
+        return $this->render(
+            'TwigBundle:Exception:error.details.html.twig',
+            [
+                'status_code' => $code,
+                'status_text' => $statusText,
+                'exception_message' => $exceptionMessage,
+            ]
+        );
+    }
 }
